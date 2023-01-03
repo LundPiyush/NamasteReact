dac1ba1ed105d27117ccf088891251a4db296319
Piyush Lund <piyushlund349@gmail.com>
2023-01-04 02:14:58 +0530

    feat(): added JSX headind component

--- LayingOutTheFoundation-Chp3/App.js
+++ LayingOutTheFoundation-Chp3/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from'react-dom';
+import ReactDOM from 'react-dom';
 
 /*
 Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”)
@@ -10,11 +10,34 @@ Composition of Component(Add a component inside another)
 {TitleComponent} vs <TitleComponent /> vs <TitleComponent><TitleComponent /> 
 */
 
-const root = ReactDOM.createRoot(document.getElementById("#root"));
-const heading1 = React.createElement("h1",{},"Heading1");
-const heading2 = React.createElement("h2",{},"Heading2");
-const heading3 = React.createElement("h3",{},"Heading3");
+// Nested elements using React.createElement
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading1 = React.createElement("h1", {}, "Heading1");
+const heading2 = React.createElement("h2", {}, "Heading2");
+const heading3 = React.createElement("h3", {}, "Heading3");
 
-const container = React.createElement("div",{},[heading1,heading2,heading3]);
+const container = React.createElement("div", {}, [heading1, heading2, heading3]);
 
-root.render(container);
+// Nested elements using JSX
+const head1 = (
+    <h1 key="head1">Heading1</h1>
+);
+
+const head2 = (
+    <h2 key="head2">Heading2</h2>
+);
+
+const head3 = (
+    <h3 key="head3">Heading3</h3>
+);
+
+const HeadingComponent = () => (
+    <div>
+        {head1}
+        {head2}
+        {head3}
+    </div>
+
+)
+
+root.render(<HeadingComponent />);

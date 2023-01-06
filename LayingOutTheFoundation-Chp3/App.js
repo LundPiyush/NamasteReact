1a51c890f76cc2e357d46242cb7dcbedf66766fd
Piyush Lund <piyushlund349@gmail.com>
2023-01-06 17:06:10 +0530

    feat(): added the React,JSX,component composition

--- LayingOutTheFoundation-Chp3/App.js
+++ LayingOutTheFoundation-Chp3/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /*
 Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”)
@@ -12,32 +12,87 @@ Composition of Component(Add a component inside another)
 
 // Nested elements using React.createElement
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading1 = React.createElement("h1", {}, "Heading1");
-const heading2 = React.createElement("h2", {}, "Heading2");
-const heading3 = React.createElement("h3", {}, "Heading3");
+const heading1 = React.createElement("h1", { key: "heading1" }, "Heading1");
+const heading2 = React.createElement("h2", { key: "heading2" }, "Heading2");
+const heading3 = React.createElement("h3", { key: "heading3" }, "Heading3");
 
-const container = React.createElement("div", {}, [heading1, heading2, heading3]);
+const container = React.createElement("div", { className: "title" }, [
+  heading1,
+  heading2,
+  heading3,
+]);
 
 // Nested elements using JSX
-const head1 = (
-    <h1 key="head1">Heading1</h1>
+const head1 = <h1 key="head1">Heading 1 JSX</h1>;
+
+const head2 = <h2 key="head2">Heading 2 JSX</h2>;
+
+const head3 = <h3 key="head3">Heading 3 JSX</h3>;
+
+const header_jsx = (
+  <div className="title">
+    {head1}
+    {head2}
+    {head3}
+  </div>
 );
 
-const head2 = (
-    <h2 key="head2">Heading2</h2>
-);
+const H1Component = () => {
+  return (
+    <h1 key="h1component" className="header1">
+      Heading1 Component
+    </h1>
+  );
+};
+const H2Component = () => {
+  return (
+    <h2 key="h2component" className="header2">
+      Heading2 Component
+    </h2>
+  );
+};
+const H3Component = () => {
+  return (
+    <h3 key="h3component" className="header3">
+      Heading3 Component
+    </h3>
+  );
+};
 
-const head3 = (
-    <h3 key="head3">Heading3</h3>
-);
-
-const HeadingComponent = () => (
-    <div>
-        {head1}
-        {head2}
-        {head3}
+// using functional component
+const TitleComponent = () => {
+  return (
+    <div key="div" className="title">
+      <H1Component />
+      <H2Component />
+      <H3Component />
     </div>
+  );
+};
 
-)
+// component composition
+const NestedComponent = () => {
+  return (
+    <div>
+      {container}
+      {header_jsx}
+      {/* {TitleComponent} It does not work and warning is thrown in console since TitleComponent is a component and not a react element*/}
+      {/* {TitleComponent()} It works since TitleComponent is normal js function  */}
+      {<TitleComponent />}
+      {/* {<TitleComponent></TitleComponent>} It works since TitleComponent is enclosed within <> */}
+    </div>
+  );
+};
 
-root.render(<HeadingComponent />);
+const HeaderComponent = () => <div></div>;
+// task 1
+//root.render(container);
+
+// task 2
+//root.render(header_jsx);
+
+//task 3
+//root.render(<NestedComponent />);
+
+// task 4
+root.render(<HeaderComponent />);
